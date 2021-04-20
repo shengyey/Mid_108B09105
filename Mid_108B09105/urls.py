337504bc77fcd44ffa0148334348fb7374ceac01
CJCU-CC <108B09105@mailst.cjcu.edu.tw>
2021-04-20 10:34:25 +0800

    2nd Commit

--- Mid_108B09105/urls.py
+++ Mid_108B09105/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('<int:pid>/<str:del_pass>', views.index),
+    path('list/', views.listing),
+    path('post/', views.posting),
+    path('contact/', views.contact),
+    path('post2db/', views.post2db),
 ]
